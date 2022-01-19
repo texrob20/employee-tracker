4a3e99848f0da68572391c448f445673c5433704
Robert C <robert.clarady@gmail.com>
2022-01-18 19:18:28 -0600

    initial CLI interaction coded

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
 const express = require('express');
 const inquirer = require('inquirer');
+
+inquirer.prompt ([{
+  type: 'list',
+  message: "Please select which you would like to do.",
+  choices: ['', ''],
+  name: 'choice'
+}])
+.then((answers) => {
+
+})
+.catch((error) => {
+    
+})
